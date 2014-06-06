1b7ece1d5ccad6020c6aadd79fc43725ce8f0157
Hashrocket Workstation <dev@hashrocket.com>
2014-06-06 19:20:40 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Artistlibrary::Application.config.secret_key_base = 'd14b5f4487a30f8832a67f33cdc7f13a2bf0e58514da1a41910a0895d2ea208433ddb70494941e9965366dafe3d86599a99583adfde619f78971df0d3552377d'
---
+++ config/routes.rb
@@ -0,0 +1,3 @@
+Artistlibrary::Application.routes.draw do
+  root 'pages#home'
+end
