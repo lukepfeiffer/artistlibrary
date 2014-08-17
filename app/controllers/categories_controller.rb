f03932bf20f065171c2e704eed2813595a1ecd50
Hashrocket Workstation <dev@hashrocket.com>
2014-08-16 22:42:01 -0400

    User views categories

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,4 +1,25 @@
 class CategoriesController < ApplicationController
   def index
+    @category = Category.all
+  end
+
+  def new
+    @category = Category.new
+  end
+
+  def create
+    @category = Category.new(category_params)
+    if @category.save
+      redirect_to categories_path
+    else
+      redirect_to new_category_path
+    end
+  end
+
+  def category_params
+    params.require(:category).permit(
+      :category,
+      :user_id
+    )
   end
 end
