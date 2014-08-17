4cc0f4349b9cfb721380b36deece5bc8157909d7
Hashrocket Workstation <dev@hashrocket.com>
2014-08-16 22:18:03 -0400

    Category link fix

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -7,7 +7,7 @@ class PagesController < ApplicationController
     if params[:tag].present?
       @images = Image.joins(:tags).where('tags.name = ?',  params[:tag]).paginate(page: params[:page], per_page: '12')
     else
-      @images = Image.find(:all, order: :id, limit: '12', order: 'created_at DESC').reverse
+      @images = Image.find(:all, order: :id, limit: '12', order: 'created_at DESC')
     end
   end
 end
